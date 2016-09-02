07240db3cfae3787596cb63d9fd7a1fcbb81a75b
Princess Sampson <ps@kingonomics.com>
2016-09-02 11:13:56 -0400

    Initial Commit

---
+++ SinglePhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SinglePhoto
+//
+//  Created by Princess Sampson on 9/2/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SinglePhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SinglePhoto
+//
+//  Created by Princess Sampson on 9/2/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
